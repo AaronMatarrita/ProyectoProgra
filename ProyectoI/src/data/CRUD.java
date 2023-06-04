c6ea2b8292662837aa4d871872823299c8fe3aca
Aaron <Aaron@Laptop>
2023-06-04 16:27:55 -0600

    Commit2

--- ProyectoI/src/data/CRUD.java
+++ ProyectoI/src/data/CRUD.java
@@ -19,7 +19,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
+//commit+-+-+-+
 public class CRUD {
 
 	public Document document;
