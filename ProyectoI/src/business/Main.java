76afd47d2edc104fc3ce226383c22106dc41b916
Aaron <Aaron@Laptop>
2023-06-27 05:39:34 -0600

    s

--- ProyectoI/src/business/Main.java
+++ ProyectoI/src/business/Main.java
@@ -1,13 +1,7 @@
 package business;
 
 public class Main {
-    public static void main(String[] args) {
-    	//new TableSortFilter();
-      new ControllerLogin();
-//    	LogicExecuteHTML log = new LogicExecuteHTML();
-//    	
-//    	String url = System.getProperty("user.dir") + "\\Manual\\Administrador\\index.html"; 
-//
-//    	log.executeHTML(url);
-    }
+	public static void main(String[] args) {
+		new ControllerLogin();
+	}
 }
